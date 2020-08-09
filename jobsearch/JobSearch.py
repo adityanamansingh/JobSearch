170cfae0ab6cfc5fe8ae9baa8a6de5a18591f89b
Jacob Azevedo Jr <39184485+jacobazevedojr@users.noreply.github.com>
2020-08-09 00:17:21 -0700

    Add files via upload

--- jobsearch/JobSearch.py
+++ jobsearch/JobSearch.py
@@ -1,64 +1,109 @@
 import requests
 from bs4 import BeautifulSoup
+from typing import List, Tuple, Dict
+from collections import Counter
+import re
 
-from typing import List, Tuple
+
+class JobPost:
+    def __init__(self, title: str, location: str, company: str, description: str):
+        self.title = title
+        self.location = location
+        self.company = company
+        self.description = description
+        # No ID attribute to improve scalability, Indeed.com uses IDs, but other job posting sites may not
+
+    def wordCounts(self):
+        # Counter object created for every individual job description
+        # Allows for blacklisting of words to discard job descriptions if they mention certain keywords
+        wordCounts = Counter()
+        if self.description is not None:
+            for word in self.description.split():
+                entry = word
+                """                
+                Regular expression used to remove all characters from the end of a word that are not
+                A-Z, a-z, 0-9, +, -, or #
+                This is done to allow words such as c++, c#, self-motivated, to be counted, but it also will
+                remove trailing punctuation or any other special characters
+                Example: "diligent," should not be counted, but "diligent" should
+                The first character in a string is also checked in case of parenthesis
+                """
+                entry = entry[1:-1] + re.sub('[^A-Za-z0-9\+\-\#]', '', entry[-1]) + re.sub('[^A-Za-z0-9]', '', entry[0])
+                entry = entry.lower()
+                wordCounts[entry] += 1
+            return wordCounts
+
 
 
 class JobSearch:
-    """
-    Initializes the JobSearch with a list of locations and positions
-    """
-    def __init__(self, locationList : List[int], positionList : List[int],  jobType: str=None , rad: int=None):
-        self.locationList = [str] * len(locationList)
-        for i, location in enumerate(locationList):
-            self.locationList[i] = location.replace(" ", "+")
+    # Initializes the JobSearch with a list of locations, positions OPTIONAL: job type and radius
+    def __init__(self, positionList : List[int], locationList : List[int], jobType: str=None , rad: int=None):
 
         self.positionList = [str] * len(positionList)
         for i, jobTitle in enumerate(positionList):
+            # Formats input for use in a search
             self.positionList[i] = jobTitle.replace(" ", "+")
 
-        # error handling for rad and jobType
+        self.locationList = [str] * len(locationList)
+        for i, location in enumerate(locationList):
+            # Formats input for use in a search
+            self.locationList[i] = location.replace(" ", "+")
+
+        # Error handling for rad and jobType
         """
         radii = [5, 15, 25, 50, 100]
         if rad not in radii:
-        
         """
         self.rad = rad
 
         """
         jobs = ["internship", "fulltime", "parttime", "temporary", "contract"]
         if jobType not in jobs:
-        
         """
         self.jobType = jobType
+        self.wordCounter = Counter()
 
-        indeedURL = []
-        if self.rad is None and self.jobType is None:
-            for i, jobTitle in enumerate(self.positionList):
-                for j, location in enumerate(self.locationList):
-                    indeedURL.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}")
-        elif self.rad is None:
-            for i, jobTitle in enumerate(self.positionList):
-                for j, location in enumerate(self.locationList):
-                    indeedURL.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}&jt={self.jobType}")
-        else:
-            for i, jobTitle in enumerate(self.positionList):
-                for j, location in enumerate(self.locationList):
-                    indeedURL.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}&radius={self.rad}&jt={self.jobType}")
+        indeedURLs = self._makeIndeedURLs()
 
-        r = requests.get(indeedURL[0])
-        print(indeedURL[0])
-        soup = BeautifulSoup(r.content, "html.parser")
-        results = soup.find(id="resultsCol")
-        print(results.prettify())
-        # look for <td id="resultsCol">
-        # nested is <a id="jobPostingsAnchor" tabindex="-1"></a> containing all jop postings
-        # All jobs are contained like with class="jobsearch-SerpJobCard unifiedRow row result clickcard"
-        # Each job has their own CSS selector which acts as a unique identifier
-        #for URL in indeedURL:
-            #r = requests.get(URL)
-            #print(r.text)
+        resultCount = 0
+        self.indeedJobIsSeen = set()
 
+        for URL in indeedURLs:
+            # Builds a list of jobs for a set family of URLs
+            jobPostings = self._getJobPosts(URL)
+            for job in jobPostings:
+                # Loading in console
+                resultCount += 1
+                print(' . ', end='')
+                if resultCount % 20 == 0:
+                    print()
+
+                jobID = job["id"][2:]
+                # If we have not seen the job before, parse the JobPost
+                if jobID not in self.indeedJobIsSeen:
+                    self.indeedJobIsSeen.add(jobID)
+
+                    """               
+                    Parses a single indeed job and returns JobPost object
+                    jobPost could be stored as a class attribute in a list of JobPost Objects for future reference
+                    if needed
+                    """
+                    jobPost = self._parseIndeedJob(job, jobID)
+
+                    if jobPost is not None:
+                        jobWordCounter = jobPost.wordCounts()
+
+                        self.JSBlacklist = []
+                        blacklisted = False
+                        for word in self.JSBlacklist:
+                            if word in jobWordCounter:
+                                blacklisted = True
+
+                        # If the job description contains a blacklisted word, the words in its descriptions are not
+                        # not counted
+                        if not blacklisted:
+                            self.wordCounter += jobWordCounter
+        print('\n', "Count:", resultCount)
 
     """
     Returns a list of pairs of the most frequently used words in the job descriptions related to the specified
@@ -83,9 +128,100 @@ class JobSearch:
     rad: 5, 10, 15, 25, 50, 100
     jobType: internship, fulltime, parttime, temporary, contract
     """
-    def wordFrequency(self, displayCount: int=-1) -> List[Tuple[str, int]]:
-        pass
+    # Intended for internal use to modularize code
+    def _parseIndeedJob(self, job: "bs4.elements.Tag", jobID: str) -> JobPost:
+        # Each job will have their information stored in the tags and classes listed below
+        # job is a Tag object in the BS4 library, with an attribute "id", which is the unique specifier for each job
+        # Each "id" is a CSS selector to allow for organization of different elements in the design
+        # First two characters "p_" from id attribute are stripped to allow for appending into URL
+        jobTitle = job.find("a", class_="jobtitle")
+        jobCompany = job.find("span", class_="company")
+        jobLocation = job.find('span', class_="location")
 
+        # job is a Tag object in the BS4 library, with an attribute "id", which is the unique specifier for each job
+        # Each "id" is a CSS selector to allow for organization of different elements in the design
+        # First two characters "p_" from id attribute are stripped to allow for appending into URL
+
+        indeedJobURL = f"https://www.indeed.com/viewjob?jk={jobID}"
+        indeedJobHTML = requests.get(indeedJobURL)
+        indeedJobSoup = BeautifulSoup(indeedJobHTML.content, "html.parser")
+        jobDescription = indeedJobSoup.find("div", id="jobDescriptionText")
+
+        if None in [jobTitle, jobCompany, jobLocation, jobDescription]:
+            return None
+
+        return JobPost(jobTitle.text.strip(), jobCompany.text.strip(), jobLocation.text.strip(), jobDescription.text)
+
+    def _makeIndeedURLs(self):
+        indeedURLs = []
+        if self.rad is None and self.jobType is None:
+            for i, jobTitle in enumerate(self.positionList):
+                for j, location in enumerate(self.locationList):
+                    indeedURLs.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}")
+        elif self.jobType is None:
+            for i, jobTitle in enumerate(self.positionList):
+                for j, location in enumerate(self.locationList):
+                    indeedURLs.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}&radius={self.rad}")
+        elif self.rad is None:
+            for i, jobTitle in enumerate(self.positionList):
+                for j, location in enumerate(self.locationList):
+                    indeedURLs.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}&jt={self.jobType}")
+        else:
+            for i, jobTitle in enumerate(self.positionList):
+                for j, location in enumerate(self.locationList):
+                    indeedURLs.append(f"https://www.indeed.com/jobs?q={jobTitle}&l={location}&radius={self.rad}&jt={self.jobType}")
+
+        return indeedURLs
+
+    def _getJobPosts(self, URL: str) -> List["bs.elements.Tag"]:
+        jobPostings = []
+        # returns entire HTML text for given URL
+        indeedJSHTML = requests.get(URL)
+        # Creates a BeautifulSoup object for HTML parsing with the 'html.parser' in the Python library
+        indeedJSSoup = BeautifulSoup(indeedJSHTML.content, "html.parser")
+        jobCount = int(indeedJSSoup.find("div", id="searchCountPages").text.strip().split()[3])
+        # Each page has 15 jobs, jobCount // 15 returns the number of pages - 1 if jobCount % 15 != 0
+        resultsPerPage = 15
+        pages = int(jobCount) // resultsPerPage
+        # If the jobCount is not divisible by 15, such as: 14, 16 etc, 1 additional page must be added
+        if jobCount % resultsPerPage != 0:
+            pages += 1
+
+        # Search position is denoted by the formula (page - 1) * 10
+        # It's incorrect, but it is Indeed.com's convention, &start=10 * n
+        startNumber = 10
+        for searchPosition in range(0, pages * startNumber, startNumber):
+            # The first page doesn't have anything additional appended to it on indeed.com
+            if searchPosition == 0:
+                newURL = URL
+            # Each subsequent page
+            else:
+                newURL = URL + f"&start={searchPosition}"
+
+            indeedJSHTML = requests.get(newURL)
+            indeedJSSoup = BeautifulSoup(indeedJSHTML.content, "html.parser")
+            # Parses HTML text for resultsCol, or where the job results for the URL are found
+            results = indeedJSSoup.find(id="resultsCol")
+            # Returns a list of jobPostings for the specified URL
+            jobPostings += results.find_all("div", class_="jobsearch-SerpJobCard")
+
+        return jobPostings
+
+    def getWordFrequency(self, displayCount: int = 0, blacklist: List[str] = []) -> List[Tuple[str, int]]:
+        frequencyCounts = Counter()
+        if displayCount < 1:
+            for key, value in self.wordCounter.most_common():
+                if key not in blacklist:
+                    frequencyCounts[key] = value
+            return frequencyCounts.most_common()
+
+        for key, value in self.wordCounter.most_common(displayCount):
+            if key not in blacklist:
+                frequencyCounts[key] = value
+        return frequencyCounts.most_common()
+
+    def getWord(self, word: List[str]) -> int:
+        return self.wordCounter[word.lower()]
     """
     resumeWeights is a txt document listing key words and relative weights
     
@@ -103,5 +239,8 @@ class JobSearch:
         pass
 
 
-js = JobSearch(["San Francisco, CA", "San Jose, CA", "Long Beach, CA"], ["Software Engineer Intern", "Software Developer Intern"], "internship")
-js.wordFrequency()
+js = JobSearch(["Software Engineer Intern", "Software Developer Intern"], ["San Francisco, CA", "San Jose, CA", "Long Beach, CA"], "internship")
+js.getWordFrequency()
+counts = js.wordCounter
+print(counts)
+print("C++:", js.getWord("python"))
